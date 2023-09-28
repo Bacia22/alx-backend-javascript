fe9dee018c47c8211f2402174b688f70baae3e1c
Bacia22 <adolfobacia@yahoo.com>
2023-09-28 23:03:08 +0000

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,14 +1,15 @@
 export default class HolbertonCourse {
-	constructor(name, length, students) {
-		this.name = name;
-		this.length = length;
-		this.students = students;
-	}
-	get name() {
-		return this._name;
-	}
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
 
-	  set name(name) {
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
     if ((typeof name !== 'string') && !(name instanceof String)) {
       throw new TypeError('Name must be a string');
     }
