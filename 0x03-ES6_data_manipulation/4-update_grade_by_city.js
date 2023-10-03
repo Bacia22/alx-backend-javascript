c396af2edec645a9a4e47325d95e3f7908c785ff
Bacia22 <adolfobacia@yahoo.com>
2023-10-03 23:52:36 +0000

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,19 @@
-/*eslint-disable */
+/* eslint-disable */
 function updateStudentGradeByCity(students, city, newGrades) {
-	const studentsInCity = students.filter((student) => student.location === city);
-	return studentsInCity.map((student) => {
-		const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
-		if (filteredGrades.length > 0) {
-			return {
-				...student,
-				grade: filteredGrades[0].grade,
-			};
-		}
-		return {
-			...student,
-			garde: 'N/A',
-		};
-	});
-}
-
-export default updateStudentGradeByCity;
+    const studentsInCity = students.filter((student) => student.location === city);
+    return studentsInCity.map((student) => {
+      const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
+      if (filteredGrades.length > 0) {
+        return {
+          ...student,
+          grade: filteredGrades[0].grade,
+        };
+      }
+      return {
+        ...student,
+        grade: 'N/A',
+      };
+    });
+  }
+  
+  export default updateStudentGradeByCity;
