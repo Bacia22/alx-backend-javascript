8843322d6eae9d11263854a665259463224a1549
Bacia22 <adolfobacia@yahoo.com>
2023-10-04 00:06:17 +0000

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,4 @@
-const creatInt8Typedarry = (length, position, value) => {
+const createInt8TypedArry = (length, position, value) => {
 	const buffer = new ArrayBuffer(length);
 	const int8Array = new Int8Array(buffer);
 	if (position > int8Array.length) throw new Error('Position outside range');
