c4bdf00ef38ba5237dd7fb1f907f737edc602841
Bacia22 <adolfobacia@yahoo.com>
2023-10-04 01:19:23 +0000

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,7 +4,7 @@ export const queryAPI = (endpoint) => {
 	if (weakMap.has(endpoint)) {
 		const endpointData = weakMap.get(endpoint);
 		if (endpointData >= 4) {
-			throw new Error('Endpoint load is high;);
+			throw new Error('Endpoint load is high');
 		}
 		waekMap.set(endpoint, (endpointData + 1));
 	} else {
